132b47cda1b48a05b55bc6be3485005b1bbc69dc
Edgar Girerd <edgargirerd@gmail.com>
2022-05-17 11:00:22 +0200

    New App.js with ignoreLogs + deprecated warnings corrected

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import {Image} from 'react-native'
 import {createStore, combineReducers} from 'redux';
+LogBox.ignoreLogs(['Warning: ...']);
 
 // import {Provider} from 'react-redux';
 const Tab = createBottomTabNavigator();
