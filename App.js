3b3f92ab954cfad2580a800eefaf07c5b7806569
RRKamen <raidarehouni@gmail.com>
2022-05-18 11:52:20 +0200

    update

--- App.js
+++ App.js
@@ -5,10 +5,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import {Image} from 'react-native'
-import {createStore, combineReducers} from 'redux';
+import { Image } from 'react-native'
+import { createStore, combineReducers } from 'redux';
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']);
+
 // import {Provider} from 'react-redux';
 const Tab = createBottomTabNavigator();
+
 // const store = createStore(combineReducers({}));
 const Stack = createStackNavigator();
 const Tab2 = createMaterialTopTabNavigator();
@@ -21,69 +25,63 @@ import Map from './screens/MapScreen'
 import Profile from './screens/Profile'
 import MyEventsPublic from './screens/MyEventsPublic'
 import MyRecipe from './screens/MyRecipe'
+import EditProfil from "./screens/editProfilScreen"
+import Create from "./screens/CreateEventScreen"
 
 function BottomNavigator() {
   return (
     <Tab.Navigator
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ color }) => {
-        if (route.name == 'Map') {
-          return <Image source={require('./assets/favicon.png')} />
-        } else if (route.name == 'Myevent') {
-          return <Image source={require('./assets/favicon.png')} />
-        } else if (route.name == 'Recipe') {
-          return <Image source={require('./assets/favicon.png')} />
-        } else if (route.name == 'Menu') {
-          return <Image source={require('./assets/favicon.png')} />
-        }
-      },
-      })}
-    tabBarOptions={{
-      activeTintColor: '#',
-      inactiveTintColor: '#',
-      style: {
-        backgroundColor: '#',
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => {
+          if (route.name == 'Map') {
+            return <Image source={require('./assets/favicon.png')} />
+          } else if (route.name == 'Myevent') {
+            return <Image source={require('./assets/favicon.png')} />
+          } else if (route.name == 'Recipe') {
+            return <Image source={require('./assets/favicon.png')} />
+          } else if (route.name == 'Menu') {
+            return <Image source={require('./assets/favicon.png')} />
+          }
+        },
+
+        // Warning corrected due to previous code deprecated
+        tabBarActiveTintColor: '#000000', //! Use a real color code, not only #
+        tabBarInactiveTintColor: '#000000',
+        tabBarStyle: [{
+          backgroundColor: '#ffffff',
+          display: "flex"
+        },
+          null
+        ]
       }
-    }}
-  >
-    <Tab.Screen name="Map" component={Map} />
-    <Tab.Screen name="Myevent" component={MyEvent} />
-    <Tab.Screen name="Recipe" component={Recipe} />
-    <Tab.Screen name="Menu" component={Menu} />
-  </Tab.Navigator>
-);
+      )}
+
+    >
+      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Myevent" component={MyEvent} />
+      <Tab.Screen name="Recipe" component={Recipe} />
+      <Tab.Screen name="Menu" component={Menu} />
+    </Tab.Navigator>
+  );
 }
-
-// function ProfileTab() {
-//   return (
-
-//   <Tab2.Navigator>
-
-//     <Tab2.Screen name="Profile" component={Profile} />
-//     <Tab2.Screen name="MyEventsPublic" component={MyEventsPublic} />
-//     <Tab2.Screen name="MyRecipe" component={MyRecipe} />
-
-//   </Tab2.Navigator>
-
-
-//   );
-// }
-
 
 
 export default function App() {
-return (
+  return (
 
     <NavigationContainer>
       <Stack.Navigator >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="ProfileFromMenu" component={Profile} />
         <Stack.Screen name="MyEventsPublic" component={MyEventsPublic} />
-        <Stack.Screen name="MyRecipe" component={MyRecipe}  />
-        {/* <Stack.Screen name="ProfileTab" component={ProfileTab} /> */}
+        <Stack.Screen name="MyRecipe" component={MyRecipe} />
         <Stack.Screen name="BottomNavigator" component={BottomNavigator} />
+        <Stack.Screen name="editProfil" component={EditProfil} />
+        <Stack.Screen name="Create" component={Create} />
+
+
       </Stack.Navigator>
     </NavigationContainer>
 
-);
+  );
 }
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,64 +1,146 @@
 import React, { useState, useEffect } from "react";
-import { View } from 'react-native';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
+import { View, KeyboardAvoidingView } from "react-native";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["Warning: ..."]);
 
 // Import map & marker
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
 
 // Import search autocomplete lib
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete' // npm install react-native-google-places-autocomplete (if issues with npm add --legacy-peer-deps)
-const GOOGLE_PLACES_API_KEY = 'AIzaSyAp9YjV01lOFf3PSsV5trlihOM4HvLc5ZA'; // never save your real api key in a snack!
-
-
-
-
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"; // npm install react-native-google-places-autocomplete (if issues with npm add --legacy-peer-deps)
+const GOOGLE_PLACES_API_KEY = "AIzaSyAp9YjV01lOFf3PSsV5trlihOM4HvLc5ZA"; // never save your real api key in a snack!
 
 export default function Map() {
-
   const [location, setLocation] = useState({ lat: 0, long: 0 });
-
+  const [listPins, setListPins] = useState([]);
+  var places;
+  var pinsAroundMe = [];
 
   // Load map + location on loading of the screen
   useEffect(() => {
+
+    let mounted = true;
+
+    // Get our location
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status == "granted") {
+      if (status == "granted" && mounted) {
         Location.watchPositionAsync({ distanceInterval: 10 }, (location) => {
           setLocation({
             lat: location.coords.latitude,
             long: location.coords.longitude,
           });
         });
+
+         //? Fetch places from backend route /nearby-places
+        var rawResponse = await fetch(
+          "http://172.16.190.148:3000/nearby-places",
+          {
+            method: "GET",
+          }
+        );
+        places = await rawResponse.json();
+        setListPins([places]);
+  
+        // for (let i = 0; i < listPins.length; i++) {
+        //   console.log("Data from fetch: ", listPins[i].placeName);
+        // }
       }
     })();
+
+    // Cleanup function
+    return () => mounted = false
+
   }, []);
+
+console.log("------List of places fetched from back: ",listPins,"------");
+
+
+
+  //? Display pins of nearby places around us on the map 
+  pinsAroundMe = listPins.map((Pin, i) => {
+    console.log("------Nearby place marker: ", Pin.places[i].coordinate.latitude, "------");
+    
+    return (
+      <Marker
+        coordinate={{
+          latitude: Pin.places[i].coordinate.latitude,
+          longitude: Pin.places[i].coordinate.longitude,
+        }}
+        title={Pin.places[i].placeName}
+        description={Pin.places[i].placeId}
+        pinColor="#5c49eb"
+        key={i}
+      />
+    );
+  });
+
+  console.log("------Pins around me:",pinsAroundMe,"------")
+
+
+
+    // //! Second solution to display pins of nearby places around us on the map 
+    // var pinsAroundMe = listPins.map((Pin, index) => {
+    //   console.log("------List of pins : ", Pin, "------");
+  
+    //   for(let markers in Pin){
+    //     console.log("------Markers in Pin: ", Pin[markers], "------");
+  
+    //     console.log("------Pin length: ",Pin[markers].length,"------" )
+    //     for(let i=10; i< 15; i++){
+    //       console.log("------",i,"------")
+    //       console.log("------Pin[markers][i]",Pin[markers][i],"------")
+    //       console.log("------Marker in markers array: ", Pin[markers][0], "------");
+    //       return (
+    //         <Marker
+    //           coordinate={{
+    //             latitude: Pin[markers][i].coordinate.latitude,
+    //             longitude: Pin[markers][i].coordinate.longitude,
+    //           }}
+    //           title={Pin[markers][i].placeName}
+    //           description={Pin[markers][i].placeId}
+    //           pinColor="#5c49eb"
+    //           key={index}
+    //         />
+    //       );
+    //       // console.log("------ Latitude:", Pin[markers][i].coordinate.latitude, "------")
+    //       // console.log("------ Name:", Pin[markers][i].placeName, "------")
+    //       // console.log("------ Name:", Pin[markers][i].placeId, "------")
+    //     }
+        
+    //   }
+  
+    //  console.log("------Pins around me:",pinsAroundMe,"------")
+      
+    // });
 
 
 
   return (
-
     <View
       style={{
         flex: 1,
       }}
     >
-
-
       <GooglePlacesAutocomplete
         //autocomplete input
+        style={{
+          flex: 1,
+          position: "absolute",
+        }}
         placeholder="Search"
         query={{
           key: GOOGLE_PLACES_API_KEY,
-          language: 'en', // language of the results
+          language: "en", // language of the results
         }}
         onPress={(data, details = null) => console.log(data)}
         onFail={(error) => console.error(error)}
         requestUrl={{
-          url:
-            'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api',
-          useOnPlatform: 'web',
+          url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api",
+          useOnPlatform: "web",
         }} // this in only required for use on the web. See https://git.io/JflFv more for details.
       />
 
@@ -76,14 +158,10 @@ export default function Map() {
           title="Hi"
           description="You are here"
           pinColor="#eb3467"
-          style={{ "width": 250, "height": 50 }}
+          style={{ width: 250, height: 50 }}
         />
-
-
-
+        {pinsAroundMe}
       </MapView>
     </View>
-
-
   );
 }
