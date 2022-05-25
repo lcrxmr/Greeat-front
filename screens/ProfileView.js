a093e78a67631bd81d4c28cd57b565e952da05d7
Amaury Lacroix <amaurylacroix@MacBook-Air-de-Amaury.local>
2022-05-25 17:56:26 +0200

    ça marche

--- screens/ProfileView.js
+++ screens/ProfileView.js
@@ -4,13 +4,13 @@ import { Image } from "react-native";
 import React, { useState } from "react";
 import Carousel from "simple-carousel-react-native";
 import ProfilePic from "../components/profilepic";
-
+import Svg, { G, Path } from "react-native-svg";
 
 export default function Profile() {
-
   const [greeat, setGreeat] = useState(0);
-  const [switchEventsButtonBgColor, setSwitchEventsButtonBgColor] = useState("#A8DD62");
-  const [greeatClick, setGreeatClick] = useState(false)
+  const [switchEventsButtonBgColor, setSwitchEventsButtonBgColor] =
+    useState("#A8DD62");
+  const [greeatClick, setGreeatClick] = useState(false);
   const [greatButtonBgColor, setGreeatButtonBgColor] = useState("#476A70");
 
   var onPressGreat = () => {
@@ -26,68 +26,97 @@ export default function Profile() {
 
   return (
     <ScrollView style={{ flex: 1 }}>
-      <View style={{ alignItems: "center" }}>
-        
-        <View style={{ flexDirection: "row", marginTop: 10 }}>
-          <Text style={{flex:'flex-start', marginLeft: 20, marginBottom: 20 , fontWeight: "600", fontSize: 14 }}>John Doe </Text>
-          <Text
-            style={{ marginLeft: "auto", marginRight: 20, marginBottom: 20 }}
-          >
-            Greeats: {greeat}
-            {/* //voir le onPress plus bas pour la valeur de 'greeat'  */}
-          </Text>
-        </View>
-        <Carousel >
-          <View>
-          <Image
-          source={require('../assets/photo1.jpg')
-          }
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 10,
+          justifyContent: "center",
+          alignItems: "space-around",
+        }}
+      >
+        <Text
           style={{
-            width: "100%",
-            height: 500,
+            marginLeft: 20,
+            marginBottom: 10,
+            fontWeight: "400",
+            fontSize: 20,
+            color: "#476A70",
           }}
-        />
+        >
+          John Doe{" "}
+        </Text>
+        <Text
+          style={{
+            marginLeft: "auto",
+            marginRight: 20,
+            marginBottom: 10,
+            fontWeight: "400",
+            opacity: 0.4,
+          }}
+        >
+          Greeats: {greeat}
+          {/* //voir le onPress plus bas pour la valeur de 'greeat'  */}
+        </Text>
+      </View>
+      <View style={{ alignItems: "center", borderRadius: 15 }}>
+        <Carousel
+          color={"#A8DD62"}
+          dimmedColor={"#476A70"}
+          backgroundColor={null}
+          backgroundRadius={15}
+          style={{ padding: 5 }}
+        >
+          <View style={{ borderRadius: 20 }}>
+            <Image
+              source={require("../assets/photo1.jpg")}
+              style={{
+                width: "100%",
+                height: 340,
+                borderRadius: 20,
+              }}
+            />
           </View>
 
           <View>
-          <Image
-          source={require('../assets/photo2.jpg')}
-          style={{
-            width: "100%",
-            height: 500,
-          }}
-        />
+            <Image
+              source={require("../assets/photo2.jpg")}
+              style={{
+                width: "100%",
+                height: 340,
+                borderRadius: 20,
+              }}
+            />
           </View>
 
           <View>
-          <Image
-          source={require('../assets/photo3.jpg')}
-          style={{
-            width: "100%",
-            height: 500,
-          }}
-        />
+            <Image
+              source={require("../assets/photo3.jpg")}
+              style={{
+                width: "100%",
+                height: 340,
+                borderRadius: 20,
+              }}
+            />
           </View>
 
           <View>
-          <Image
-          source={require('../assets/photo4.jpg')}
-          style={{
-            width: "100%",
-            height: 500,
-          }}
-        />
+            <Image
+              source={require("../assets/photo4.jpg")}
+              style={{
+                width: "100%",
+                height: 340,
+                borderRadius: 20,
+              }}
+            />
           </View>
         </Carousel>
-
-
 
         <Text
           style={{
             marginLeft: 20,
             marginRight: 20,
             marginBottom: 20,
-            marginTop: 10,
+            marginTop: 40,
           }}
         >
           Un paragraphe est une section de texte en prose vouée au développement
@@ -114,12 +143,43 @@ export default function Profile() {
             backgroundColor: switchEventsButtonBgColor,
             backgroundColor: greatButtonBgColor,
             borderRadius: 25,
+            marginTop: 50,
           }}
+          iconRight={true}
+          icon={
+            <Svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={13.146}
+              height={11.888}
+              marginLeft={20}
+              viewBox="0 0 13.146 11.888"
+            >
+              <G
+                id="Group_947"
+                data-name="Group 947"
+                transform="translate(-370.998 -32.764)"
+              >
+                <G
+                  id="Group_14"
+                  data-name="Group 14"
+                  transform="translate(370.998 32.764)"
+                >
+                  <Path
+                    id="Path_41"
+                    data-name="Path 41"
+                    d="M384,35.792a4.1,4.1,0,0,0-1.379-2.187,3.655,3.655,0,0,0-2.3-.842A3.6,3.6,0,0,0,377.57,34.1a3.6,3.6,0,0,0-2.757-1.335,3.653,3.653,0,0,0-2.3.842,4.1,4.1,0,0,0-1.379,2.187,5.368,5.368,0,0,0,.847,4.262,13.987,13.987,0,0,0,5.3,4.524.6.6,0,0,0,.572,0,14,14,0,0,0,5.3-4.524A5.367,5.367,0,0,0,384,35.792Zm-6.434,7.579"
+                    transform="translate(-370.998 -32.764)"
+                    fill="#bcea64"
+                  />
+                </G>
+              </G>
+            </Svg>
+          }
           onPress={() => {
             setGreeat(greeat + 1);
-            setSwitchEventsButtonBgColor('red')
-            onPressGreat()
-          }}  // useState implement
+            setSwitchEventsButtonBgColor("red");
+            onPressGreat();
+          }} // useState implement
         ></Button>
       </View>
     </ScrollView>
