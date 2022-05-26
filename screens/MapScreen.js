08954878cb62c21ef70262adae6965951bb74182
RRKamen <raidarehouni@gmail.com>
2022-05-26 11:00:46 +0200

    last version

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -104,7 +104,7 @@ export default function Map(props) {
       });
 
       var rawResponse = await fetch(
-        "http://172.17.188.2.2:3000/nearby-places",
+        "http://172.17.188.2:3000/nearby-places",
         {
           method: "GET",
         }
@@ -394,74 +394,140 @@ export default function Map(props) {
       ) / 1000
     ).toFixed(1);
     return (
-      <Card borderRadius={15} containerStyle={styles.card}>
-        <View style={{ flexDirection: "row" }}>
-          <View style={{ flex: 0.8 }}>
-            <Image
-              style={{ borderRadius: 10, height: 120, width: 120 }}
-              source={{ uri: events[i].image }}
-            />
-          </View>
-          <View style={{ flex: 1, alignItems: "flex-start" }}>
-            <Text
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => {
+          props.navigation.navigate("EventDetailScreen", {
+            screen: "EventDetailScreen",
+            event: e,
+          });
+        }}
+      >
+        <Card borderRadius={15} containerStyle={styles.card}>
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ flex: 0.8 }}>
+              <Image
+                style={{ borderRadius: 10, height: 120, width: 120 }}
+                source={{ uri: e.image }}
+              />
+            </View>
+
+            {/* //! ---------------------------------------------------- */}
+
+            <View
               style={{
-                paddingTop: 10,
-                fontWeight: "bold",
-                fontSize: 16,
+                flex: 1,
+                alignItems: "flex-start",
                 justifyContent: "flex-start",
               }}
             >
-              {events[i].name}
-            </Text>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Image
-                style={{ height: 18, width: 15, marginRight: 3, marginTop: 5 }}
-                source={require("../assets/location.png")}
-              />
-              <Text
+              <View
                 style={{
-                  paddingTop: 10,
-                  fontSize: 16,
-                  justifyContent: "flex-start",
-                  marginRight: 3,
-                  marginBottom: 3,
-                }}
-              >
-                {" "}
-                {dis}
-              </Text>
-              <Text
-                style={{
-                  paddingTop: 10,
-                  fontSize: 12,
+                  flex: 0.5,
+                  alignItems: "flex-start",
                   justifyContent: "flex-start",
                 }}
               >
-                Km away
-                {/* {events[i].location} */}
-              </Text>
+                <Text
+                  style={{
+                    paddingTop: 10,
+                    fontWeight: "bold",
+                    fontSize: 16,
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  {events[i].name}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 0.5,
+                  justifyContent: "flex-end",
+                  alignItems: "flex-start",
+                  paddingBottom: 5,
+                }}
+              >
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <Image
+                    style={{
+                      height: 18,
+                      width: 15,
+                      marginRight: 3,
+                      marginTop: 5,
+                    }}
+                    source={require("../assets/location.png")}
+                  />
+                  <Text
+                    style={{
+                      paddingTop: 10,
+                      fontSize: 16,
+                      justifyContent: "flex-start",
+                      marginRight: 3,
+                      marginBottom: 3,
+                    }}
+                  >
+                    {" "}
+                    {dis}
+                  </Text>
+                  <Text
+                    style={{
+                      paddingTop: 10,
+                      fontSize: 12,
+                      justifyContent: "flex-start",
+                    }}
+                  >
+                    Km away
+                  </Text>
+                </View>
+
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginLeft: -2,
+                  }}
+                >
+                  <Image
+                    style={{
+                      height: 17,
+                      width: 17,
+                      marginRight: 3,
+                      marginTop: 0,
+                    }}
+                    source={require("../assets/date.png")}
+                  />
+                  <Text
+                    style={{
+                      paddingTop: 5,
+                      fontSize: 16,
+                      justifyContent: "flex-start",
+                      marginRight: 3,
+                      marginBottom: 3,
+                    }}
+                  >
+                    {" "}
+                    {e.date}
+                  </Text>
+                  <Text
+                    style={{
+                      paddingTop: 5,
+                      fontSize: 12,
+                      justifyContent: "flex-start",
+                    }}
+                  >
+                    /5
+                  </Text>
+                </View>
+              </View>
             </View>
 
-            <Badge
-              containerStyle={{
-                flex: 1,
-                justifyContent: "flex-end",
-                marginBottom: 10,
-              }}
-              value="Voir les détails"
-              badgeStyle={{
-                backgroundColor: "#476A70",
-                height: 25,
-                borderRadius: 20,
-              }}
-              textStyle={{
-                marginLeft: 10,
-                marginRight: 10,
-              }}
-            />
+            {/* //! ---------------------------------------------------- */}
+
+
           </View>
-        </View>
-      </Card>
+        </Card>
+      </TouchableOpacity>
     );
   });
 
